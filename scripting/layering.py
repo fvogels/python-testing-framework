775100eaf219cdc029f57f48b9c58f5b36c2ecb6
Frederic Vogels <frederic.vogels@ucll.be>
2019-01-27 20:57:59 +0100

    closes #7

---
+++ scripting/layering.py
@@ -0,0 +1,38 @@
+from contextlib import contextmanager
+from scripting.dynamic import create_dynamic_variable, dynamic_bind
+from scripting.testing import observers, skip_if
+
+
+__layer = create_dynamic_variable()
+
+
+@contextmanager
+def initialize_layering():
+    with dynamic_bind(__layer, 0):
+        yield
+
+
+@contextmanager
+def layer():
+    with dynamic_bind(__layer, __layer.value + 1):
+        yield
+
+
+@contextmanager
+def layer_observers(on_pass=None, on_fail=None, on_skip=None):
+    observer_layer = __layer.value
+
+    def wrap(f):
+        def observer():
+            if f and __layer.value == observer_layer:
+                f()
+
+        return observer
+
+    on_pass = wrap(on_pass)
+    on_fail = wrap(on_fail)
+    on_skip = wrap(on_skip)
+
+    with observers(on_pass=on_pass, on_fail=on_fail, on_skip=on_skip):
+        yield
+
