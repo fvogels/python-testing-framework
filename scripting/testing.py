9c7d425c23f96b016028cf4fdb48db418b8730c8
Frederic Vogels <frederic.vogels@ucll.be>
2019-01-27 18:35:47 +0100

    closes #3

--- scripting/testing.py
+++ scripting/testing.py
@@ -2,15 +2,16 @@ from contextlib import contextmanager
 from scripting.assertions import AssertionFailure
 from scripting.dynamic import create_dynamic_variable, dynamic_bind, dynamic_append
 
-
 __pass_observers = create_dynamic_variable()
 __skip_observers = create_dynamic_variable()
 __fail_observers = create_dynamic_variable()
+__skip_predicate = create_dynamic_variable()
+
 
 
 @contextmanager
 def initialize_testing_environment():
-    with dynamic_bind(__pass_observers, []), dynamic_bind(__skip_observers, []), dynamic_bind(__fail_observers, []):
+    with dynamic_bind(__pass_observers, []), dynamic_bind(__skip_observers, []), dynamic_bind(__fail_observers, []), dynamic_bind(__skip_predicate, lambda: False):
         yield
 
 @contextmanager
@@ -22,39 +23,70 @@ def observers(on_pass=None, on_fail=None, on_skip=None):
     on_fail = on_fail or do_nothing
     on_skip = on_skip or do_nothing
 
-    with dynamic_append(__pass_observers, on_pass), dynamic_append(__fail_observers, on_fail), dynamic_bind(__skip_observers, on_skip):
+    with dynamic_append(__pass_observers, on_pass), dynamic_append(__fail_observers, on_fail), dynamic_append(__skip_observers, on_skip):
+        yield
+
+@contextmanager
+def skip_if(predicate):
+    # If necessary, turn boolean into predicate
+    if type(predicate) == bool:
+        value = predicate
+        predicate = lambda: value
+
+    previous = __skip_predicate.value
+
+    def new_predicate():
+        result = previous() or predicate()
+        return result
+
+    with dynamic_bind(__skip_predicate, new_predicate):
+        yield
+
+@contextmanager
+def skip_unless(predicate):
+    # If necessary, turn boolean into predicate
+    if type(predicate) == bool:
+        value = predicate
+        predicate = lambda: value
+
+    def negated_predicate():
+        return not predicate()
+
+    with skip_if(negated_predicate):
         yield
 
 
-def __shouldTestRun():
-    return True
+def __should_test_run():
+    # pylint: disable=E1102
+    should_skip = (__skip_predicate.value)()
+    return not should_skip
 
 
-def __testPassed():
+def __test_passed():
     for observer in __pass_observers.value:
         observer()
 
 
-def __testFailed():
+def __test_failed():
     for observer in __fail_observers.value:
         observer()
 
 
-def __testSkipped():
+def __test_skipped():
     for observer in __skip_observers.value:
         observer()
 
 
 def test():
     def receiver(f):
-        if __shouldTestRun():
+        if __should_test_run():
             try:
                 f()
-                __testPassed()
+                __test_passed()
 
             except AssertionFailure:
-                __testFailed()
+                __test_failed()
         else:
-            __testSkipped()
+            __test_skipped()
 
     return receiver
