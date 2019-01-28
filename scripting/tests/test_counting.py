5d466fd533ffcd09a6af29d02238e14b3ea4e27e
Frederic Vogels <frederic.vogels@ucll.be>
2019-01-28 15:23:42 +0100

    closes #17

--- scripting/tests/test_counting.py
+++ scripting/tests/test_counting.py
@@ -3,7 +3,7 @@ from scripting.fileutils import find_files_recursively, has_name
 from scripting.testing import test, skip_if
 from scripting.counting import keep_counts, Counts
 from scripting.assertions import fail
-from scripting.flow import skip_after_fail
+from scripting.flow import skip_after_fail, continue_after_fail
 from scripting.scoring import keep_score, all_or_nothing
 
 
@@ -114,6 +114,54 @@ class TestCounting(TestCase):
 
     def test_all_or_nothing_fpp(self):
         with keep_score(), keep_counts() as current_count, all_or_nothing():
+            @test()
+            def _():
+                fail()
+
+            @test()
+            def _():
+                pass
+
+            @test()
+            def _():
+                pass
+
+            self.assertEqual(current_count(), Counts(2, 1, 0))
+
+    def test_skip_after_fail_fpp(self):
+        with keep_counts() as current_count, skip_after_fail():
+            @test()
+            def _():
+                fail()
+
+            @test()
+            def _():
+                pass
+
+            @test()
+            def _():
+                pass
+
+            self.assertEqual(current_count(), Counts(0, 1, 2))
+
+    def test_continue_after_fail_fpp(self):
+        with keep_counts() as current_count, continue_after_fail():
+            @test()
+            def _():
+                fail()
+
+            @test()
+            def _():
+                pass
+
+            @test()
+            def _():
+                pass
+
+            self.assertEqual(current_count(), Counts(2, 1, 0))
+
+    def test_continue_after_fail_inside_skip_after_fail_fpp(self):
+        with keep_counts() as current_count, skip_after_fail(), continue_after_fail():
             @test()
             def _():
                 fail()
