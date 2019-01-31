50ffc16ff1f631699dff5b6552310c069b74d47e
Frederic Vogels <frederic.vogels@ucll.be>
2019-01-31 19:50:19 +0100

    closes #39

--- scripting/quick.py
+++ scripting/quick.py
@@ -4,6 +4,7 @@ from scripting.testing import test, skip_unless
 from scripting.assertions import assert_equal_results
 from scripting.tested import fetch_tested_implementation
 from scripting.reference import fetch_reference_implementation
+from scripting.scoring import scale
 import scripting.reference
 
 
@@ -12,5 +13,5 @@ def reference_based_test(identifier):
     ref = fetch_reference_implementation(identifier)
     tested = fetch_tested_implementation(identifier)
 
-    with scripting.reference.reference_based_test(ref, tested) as testcase, skip_unless(bool(tested)):
+    with scale(1), scripting.reference.reference_based_test(ref, tested) as testcase, skip_unless(bool(tested)):
         yield testcase
