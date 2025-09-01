4dbda6f85c651cb2148627c1b764ac09aed9f50d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-09-01 18:52:22 +0200

    cleanup

--- scripting/quick.py
+++ scripting/quick.py
@@ -1,5 +1,4 @@
 from contextlib import contextmanager
-from scripting.dynamic import create_dynamic_variable, dynamic_bind, dynamic_append
 from scripting.testing import test, skip_unless
 from scripting.assertions import assert_truthy, assert_falsey
 from scripting.tested import fetch_tested_implementation
